0b6c36180e5a8e3d077c4838f1e6ce1e8fc65c5e
Egor Talovikov <egtalovikov@gmail.com>
2023-08-12 20:18:38 +0500

    feat: сделал базовую конфигурацию сервера

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,3 +1,16 @@
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
 
 
-const
+const start = async () => {
+    try {
+        const PORT = process.env.PORT || 5001;
+        const app = await NestFactory.create(AppModule)
+
+        await app.listen(PORT, () => console.log(`server started on PORT ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
